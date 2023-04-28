e281e30345ef5fd57c5b75f8e77439630a644873
HumaIsmail-95 <58975069+HumaIsmail-95@users.noreply.github.com>
2023-04-28 16:11:47 +0500

    minimum work

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -11,7 +11,7 @@
                         <option value="3">Three</option>
                     </select>
                 </div>
-                <div  class="mx-3 border-end d-flex align-items-center">
+                <div class="mx-3 border-end d-flex align-items-center">
                     <i class="fa fa-pound-sign"></i>
                     <select class="form-select" aria-label="Default select example">
                         <option selected>English</option>
@@ -25,44 +25,43 @@
         </div>
     </div>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-
-            <a class="navbar-brand" href="#">Navbar</a>
+        <div class="container-fluid px-lg-5">
+            <a class="navbar-brand" href="#">
+                <img src="{{ asset('') }}" alt="" srcset="">
+            </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled">Disabled</a>
-                    </li>
-                </ul>
+
                 <form class="d-flex" role="search">
                     <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                     <button class="btn btn-outline-success" type="submit">Search</button>
                 </form>
+                <ul class="navbar-nav mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">
+                            <i class="far fa-user"></i>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            <i class="far fa-heart"></i>
+                        </a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link" href="#">
+                            <i class="fas fa-random"></i>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            <i class="fas fa-random"></i>
+                        </a>
+                    </li>
+                </ul>
             </div>
         </div>
     </nav>
